4cbbccb9528d7f2c7d192532afd1ed743024866a
Gamer686 <74534832+Gamer686@users.noreply.github.com>
2022-08-31 12:31:25 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,30 @@
+import csv 
+import statistics
+import plotly.express as px
+import pandas as pd
+import math
+import random
+import plotly.figure_factory as ff
+
+df = pd.read_csv("medium_data.csv")
+data = df["radeing_time"].tolist()
+fig = ff.create_displot([data], ["reading_time"], show_hist = False)
+fig.show()
+
+population_mean = statistics.mean(data)
+print("Mean =", population_mean)
+population_standarddeviation = statistics.stdev(data)
+print("Standard Deviation =" , population_standarddeviation)
+
+#Code to find mean and standard deviatin of 100 data ponts
+dataset = []
+for i in range(0,1000):
+    random_index = random.randint(0,len(data))
+    value = data[random_index]
+    dataset.append(value)
+mean = statistics.men(dataset)
+standarddeviation = statistics.stdev(dataset)
+print("Mean of 1000 vaules", mean)
+print("Standard devition of 1000 vaule", standarddeviation)
+
+
